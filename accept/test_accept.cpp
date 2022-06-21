4adcd5001f74dd11f71fe8adf4e8f93f23ebae9e
Ingo <ingo@hoeft-online.de>
2022-06-21 20:33:32 +0200

    Mock sys/socket accept system function

--- accept/test_accept.cpp
+++ accept/test_accept.cpp
@@ -7,80 +7,105 @@
 // ----------------------------------------------------------------------------
 
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include "gmock/gmock.h"
 
+using ::testing::_;
 using ::testing::DoAll;
+using ::testing::Pointee;
 using ::testing::Return;
 using ::testing::SetArgPointee;
 
-class MockedSysSocket {
+//
+class SysSocketInterface {
   public:
-    MOCK_METHOD(int, accept, (int, sockaddr*, socklen_t* addrlen), ());
+    virtual ~SysSocketInterface() {}
+    virtual int accept(int sockfd, struct sockaddr* addr,
+                       socklen_t* addrlen) = 0;
 };
 
-TEST(MockedSysSocketTest, SideEffect1) {
-    MockedSysSocket mocked_sys_socket;
+// Version to call the real system function.
+class RealSysSocket : public SysSocketInterface {
+  public:
+    virtual ~RealSysSocket() override {}
+    int accept(int sockfd, struct sockaddr* addr, socklen_t* addrlen) override {
+        return ::accept(sockfd, addr, addrlen);
+    }
+};
 
-    int accept_sock = 3;
-    int listen_sock = 4;
+// Version to call the mocked system function.
+class MockSysSocket : public SysSocketInterface {
+  public:
+    virtual ~MockSysSocket() override {}
+    MOCK_METHOD(int, accept, (int, struct sockaddr*, socklen_t*), (override));
+};
 
-    struct sockaddr_storage clientAddr {};
-    struct sockaddr* sa{(sockaddr*)&clientAddr};
-    struct sockaddr_in* sa_in{(sockaddr_in*)&clientAddr};
+//
+class SysSocket {
+    SysSocketInterface* m_syssocket;
 
+  public:
+    SysSocket(SysSocketInterface* a_syssocket) : m_syssocket(a_syssocket) {}
+
+    std::string get_addr(int a_listen_sock) {
+        struct sockaddr_storage clientAddr;
+        struct sockaddr_in* clientAddr_in{(sockaddr_in*)&clientAddr};
+        socklen_t socklen = sizeof(sockaddr_storage);
+
+        int accept_sock = m_syssocket->accept(a_listen_sock,
+                                              (sockaddr*)&clientAddr, &socklen);
+        if (accept_sock == -1) {
+            return "";
+        }
+
+        char text_addr[INET_ADDRSTRLEN];
+        if (inet_ntop(AF_INET, &clientAddr_in->sin_addr, text_addr,
+                      sizeof(text_addr)) == nullptr) {
+            return "";
+        }
+
+        unsigned short int port = ntohs(clientAddr_in->sin_port);
+        return std::string(text_addr) + ":" + std::to_string(port);
+    }
+};
+
+//
+TEST(SysSocketTestSuite, call_real_sys_socket_accept) {
+    RealSysSocket real_sys_socket;
+    // Inject real sys_socket object
+    SysSocket sys_socket(&real_sys_socket);
+
+    int listen_sock = 0;
+
+    // Test Unit
+    // Listen_sock is not a valid socket
+    EXPECT_EQ(sys_socket.get_addr(listen_sock), "");
+}
+
+TEST(SysSocketTestSuite, mock_sys_socket_accept) {
+    // Provide a socket address
     struct sockaddr_storage clientAddrMock;
-    struct sockaddr* sa_mock{(sockaddr*)&clientAddrMock};
-    struct sockaddr_in* sa_in_mock{(sockaddr_in*)&clientAddrMock};
-    sa_in_mock->sin_family = AF_INET;
-    sa_in_mock->sin_port = htons(50000);
-    inet_pton(AF_INET, "192.168.1.2", &sa_in_mock->sin_addr);
+    struct sockaddr* sa{(sockaddr*)&clientAddrMock};
+    struct sockaddr_in* sa_in{(sockaddr_in*)&clientAddrMock};
+    sa_in->sin_family = AF_INET;
+    sa_in->sin_port = htons(50000u);
+    inet_pton(AF_INET, "192.168.1.2", &sa_in->sin_addr);
+    socklen_t socklen = sizeof(sockaddr_storage);
 
-    socklen_t clientLen = sizeof(sockaddr_storage);
+    // Instantiate mocked sys_socket object
+    MockSysSocket mock_sys_socket;
+    // Inject mocked sys_socket object
+    SysSocket sys_socket(&mock_sys_socket);
 
-    EXPECT_CALL(mocked_sys_socket, accept(listen_sock, sa, &clientLen))
-        .WillOnce(DoAll(SetArgPointee<1>(*sa_mock), Return(accept_sock)));
+    int listen_sock = 3;
+    int accept_sock = 4;
 
-    auto actual = mocked_sys_socket.accept(listen_sock, sa, &clientLen);
-    EXPECT_EQ(sa_in->sin_family, AF_INET);
-    EXPECT_EQ(sa_in->sin_port, ntohs(50000));
-    char text_addr[INET_ADDRSTRLEN];
-    EXPECT_NE(
-        inet_ntop(AF_INET, &sa_in->sin_addr, text_addr, sizeof(text_addr)),
-        nullptr);
-    EXPECT_STREQ(text_addr, "192.168.1.2");
-    EXPECT_EQ(actual, accept_sock);
+    EXPECT_CALL(mock_sys_socket, accept(listen_sock, _, Pointee(socklen)))
+        .WillOnce(DoAll(SetArgPointee<1>(*sa), Return(accept_sock)));
+
+    // Test Unit
+    EXPECT_EQ(sys_socket.get_addr(listen_sock), "192.168.1.2:50000");
 }
-
-#if false
-TEST(MockedSysSocketTest, SideEffect2) {
-  MockedSysSocket mocked_sys_socket;
-
-  int accept_sock = 1;
-  int listen_sock = 2;
-
-  struct sockaddr sa_in {
-    "initial"
-  };
-  struct sockaddr sa_next {
-    "next"
-  };
-
-  socklen_t clientLen = sizeof(sockaddr);
-
-  EXPECT_CALL(mocked_sys_socket, accept(listen_sock, &sa_in, &clientLen))
-      .WillOnce(WithArg<1>(Invoke([&sa_next, accept_sock](sockaddr *in) {
-        *in = sa_next;
-        return accept_sock;
-      })));
-
-  auto actual = mocked_sys_socket.accept(listen_sock, &sa_in, &clientLen);
-  EXPECT_EQ(sa_in.name, "next");
-  EXPECT_EQ(sa_in.name, sa_next.name);
-  EXPECT_EQ(actual, accept_sock);
-}
-#endif
 //-----------------------------------------------------------------------------
 
 int main(int argc, char** argv) {
